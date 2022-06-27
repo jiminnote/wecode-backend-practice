b02775aa54695cdc1ac09e41172b6d34c885f7b5
jeongjimin <wlals3968@naver.com>
2022-06-27 18:37:13 +0900

    ADD : list만드는 중

--- product/views.py
+++ product/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.http      import JsonResponse
+from django.views     import View
+from django.db.models import Q
 
-# Create your views here.
+from .models import Product
+class ProductListView(View):
+    def get(self, request):
+        main_category = request.GET.get('main_category')
+        category      = request.GET.get('sub_category')
+        sort          = request.GET.get('sort', 'id')
+        limit         = int(request.GET.get('limit', 36))
+        offset        = int(request.GET.get('offset', 0))
+
+        q = Q()
+            
+        if main_category:
+            q &= Q(sub_category__main_category_id=main_category)
+
+        if category:
+            q &= Q(ategory_id=category)
+
+        sort_type = {
+            'id'   : 'id',
+            'rand'  : '?'
+        }
+
+        products = Product.objects.select_related('sub_category')\
+            .filter(q).order_by(sort_type[sort])[offset:offset+limit]
+
+        product_list = [{
+            'id'        : product.id,
+            'name'      : product.name,
+            'price'     : product.price
+        } for product in products]
+
+        return JsonResponse({'products_list':product_list}, status=200)

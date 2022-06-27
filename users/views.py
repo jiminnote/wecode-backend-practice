a15918342630aab919c940922763c13a9761d062
jeongjimin <wlals3968@naver.com>
2022-06-27 17:37:11 +0900

    UPDATE : skin type 제외+ 회원가입,로그인  마무리

--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ from django.core.exceptions import ValidationError
 from core.utils     import login_decorator
 from core.validator import validate_email,validate_name,validate_password
 from wesop.settings import SECRET_KEY,ALGORITHM
-from users.models   import User, Skintype
+from users.models   import User
 
 class SignupView(View): 
     def post(self,request):
@@ -18,11 +18,7 @@ class SignupView(View):
             email        = data['email'] 
             first_name   = data['first_name'] 
             last_name    = data['last_name']
-            skin_type_id = data['skin_type']
             password     = data['password']
-            
-            if not Skintype.objects.filter(skin_type_id = skin_type_id).exists():
-                return JsonResponse({"message":"INVALID_SKIN_TYPE"}, status = 400)
             
             if User.objects.filter(email = email).exists():
                 return JsonResponse({"message":"DUPLICATE_EMAIL"}, status = 400)
@@ -33,35 +29,17 @@ class SignupView(View):
             validate_password(password)
             
             User.objects.create(
-                first_name = first_name,
-                last_name  = last_name,
-                email      = email,
-                password   = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
-                skin_type_id = skin_type_id
+                first_name   = first_name,
+                last_name    = last_name,
+                email        = email,
+                password     = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
             )
             return JsonResponse({"message":"SUCCESS"}, status = 201)
         
         except KeyError:
             return JsonResponse({"message":"KEY_ERROR"}, status = 400)
         except ValidationError as e :
-            return JsonResponse({'message' : e.message }, status = 400)
-        
-    def get(self, request):
-        users = User.objects.all()
-        results = [
-                {
-                'first_name'     : user.first_name,
-                'last_name'      : user.first_name,
-                'email'          : user.email,
-                'password'       : user.password,
-                'skin_type_id'   : user.skin_type_id,
-                'skin_type_name' : user.skin_type.skin_type
-            } for user in users ]
-
-        return JsonResponse({"results" : results}, status=200)
-        # return JsonResponse({'results':list(User.objects.values())},status = 200)
-        
-    
+            return JsonResponse({'message' : e.message }, status = 400)    
 class SigninView(View):
     def post(self,request):
         try:
@@ -73,7 +51,6 @@ class SigninView(View):
                 return JsonResponse({"message":"INVALID_USER"}, status = 401)
             
             user = User.objects.get(email = email)
-            
             
             if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
                 return JsonResponse({"message": "INVALID_USER"}, status = 401)

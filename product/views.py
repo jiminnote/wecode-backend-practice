ea88326a0a5a6f33e3383d22e1f57c1dc4a36710
jeongjimin <wlals3968@naver.com>
2022-06-27 19:05:01 +0900

    ADD : list만드는 중

--- product/views.py
+++ product/views.py
@@ -6,7 +6,7 @@ from .models import Product
 class ProductListView(View):
     def get(self, request):
         main_category = request.GET.get('main_category')
-        category      = request.GET.get('sub_category')
+        category      = request.GET.get('category')
         sort          = request.GET.get('sort', 'id')
         limit         = int(request.GET.get('limit', 36))
         offset        = int(request.GET.get('offset', 0))
@@ -17,20 +17,19 @@ class ProductListView(View):
             q &= Q(sub_category__main_category_id=main_category)
 
         if category:
-            q &= Q(ategory_id=category)
+            q &= Q(category_id=category)
 
         sort_type = {
             'id'   : 'id',
             'rand'  : '?'
         }
 
-        products = Product.objects.select_related('sub_category')\
-            .filter(q).order_by(sort_type[sort])[offset:offset+limit]
+        products = Product.objects.select_related('category').filter(q).order_by(sort_type[sort])[offset:offset+limit]
 
         product_list = [{
             'id'        : product.id,
             'name'      : product.name,
-            'price'     : product.price
+            
         } for product in products]
 
         return JsonResponse({'products_list':product_list}, status=200)

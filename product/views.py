84df16b900eecb33790bf99bad960f88599c8499
jeongjimin <wlals3968@naver.com>
2022-06-29 11:11:58 +0900

    wip

--- product/views.py
+++ product/views.py
@@ -7,8 +7,10 @@ from .models import Product, Category
 class ProductListView(View):
     def get(self, request, *args, **kwargs):
         try:
-            main_category_id = request.GET.get('main_category')
+            #:8000/products?main_category_id=1&limt=10&offset=2&sort=random
+            main_category_id = request.GET.get('main_category_id')
             category_id      = request.GET.get('category_id')
+            sort             = request.GET.get('sort')
             limit            = int(request.GET.get('limit', 20))
             offset           = int(request.GET.get('offset', 0))
 
@@ -18,47 +20,42 @@ class ProductListView(View):
                 q &= Q(category__id = category_id)
                 
             if main_category_id:
-                q &= Q(main_category__id = main_category_id)
+                q &= Q(category__main_sub_category__main_category_id = main_category_id)
             
-            products = Product.objects.filter(q)[offset:offset+limit]
+            sort_set = {
+                'random' : '?'
+            }
+            
+            order_filed = sort_set.get(sort, 'id')
+            
+            products = Product.objects.filter(q).order_by(order_filed)[offset:offset+limit]
 
             result = [
                 {
-                "products" : 
-                    {
-                        "product_id"     : product.id,
-                        "product_name"   : product.name,
-                        "category_id"    : product.category.id,
-                        "category_name"  : product.category.name,
-                        "product_detail" : [
-                            {
-                                "product_option_id" : product_option.id,
-                                "size_mL"           : product_option.sizes_mL,
-                                "price"             : product_option.price,
-                                "image_url"         : product_option.image_url,
-                            } for product_option in product.productoption_set.all()
-                        ],
-                        "product_feature" : [
+                    "products" : 
                         {
-                            "id"      : feature.feature.id,
-                            "feature" : feature.feature.name,
-                            "content" : feature.content
-                            }for feature in product.Products_feature.filter(is_addtional=False)
-                        ]
+                            "product_id"     : product.id,
+                            "product_name"   : product.name,
+                            "category_id"    : product.category.id,
+                            "category_name"  : product.category.name,
+                            "product_detail" : [
+                                {
+                                    "product_option_id" : product_option.id,
+                                    "size_mL"           : product_option.sizes_mL,
+                                    "price"             : product_option.price,
+                                    "image_url"         : product_option.image_url,
+                                } for product_option in product.productoption_set.all()
+                            ],
+                            "product_feature" : [
+                                {
+                                "id"      : feature.feature.id,
+                                "feature" : feature.feature.name,
+                                "content" : feature.content
+                                }for feature in product.Products_feature.filter(is_addtional=False)
+                            ]
                         }
-                } for product in products]
+                } for product in products
+            ]
             
             return JsonResponse({"message" : result}, status = 200)
-        
-        except KeyError:
-            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
-        
-        except Category.DoesNotExist:
-            return JsonResponse({"message" : "NO_CATEGORY_FOUND"}, status = 400)
-        
-        
-        
-        
-        
-
         
--- wesop/urls.py
+++ wesop/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('users',include('users.urls')),
-    path('product',include('product.urls'))
+    path('products',include('product.urls'))
     
 ]
